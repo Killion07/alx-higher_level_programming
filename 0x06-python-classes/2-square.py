09fcbdcb96073c5155396f50b9d64605e54f8156
Killion07 <wkilion@gmail.com>
2023-01-24 18:27:26 -0800

    Size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,13 @@
 
 class Square:
     """Initializing the size to zero"""
+
     def __init__(self, size=0):
+        """
+        Args:
+            size: size of square
+        """
+
         if type(size) is int:
             if size < 0:
                 raise ValueError('size must be >= 0')
