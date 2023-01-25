d9bebfae3335d86e2aa7f4d1c14ec4db305ed1ea
Killion07 <wkilion@gmail.com>
2023-01-24 17:55:39 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class called square"""
+
+
+class square:
+    """The class has a difened size"""
+    def __init__(self, size):
+        """"We initialize the size of the square"""
+        self.size = size
