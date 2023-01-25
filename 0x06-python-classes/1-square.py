2cc3d1f2eb081fb9b61e1690508f83244f890599
Killion07 <wkilion@gmail.com>
2023-01-24 18:00:24 -0800

    Class size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,15 @@
 
 
 class square:
-    """The class has a difened size"""
+    """The class has a difened size
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size):
-        """"We initialize the size of the square"""
+        """"We initialize the size of the square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+
+        """
         self.size = size
