9d93f65d9f7fc122ba04c77cca14205b905ba183
Killion07 <wkilion@gmail.com>
2023-01-31 12:52:56 -0800

    Extra assignments

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Locked class
+"""
+
+class LockedClass:
+    """
+    Prevents user from instantiating any other class appert from first_name
+    """
+
+    __slots__ = ["first_name"]
