9e50fd172cbf3a43b43542fbe96cea9fec67e84a
Killion07 <wkilion@gmail.com>
2023-02-07 16:58:26 -0800

    json stirng

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains JSON string
+"""
+
+import json
+
+
+
+def to_json_string(my_obj):
+    """returns json representation of an object"""
+    return json.dumps(my_obj)
