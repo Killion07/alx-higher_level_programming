5e5f950a1602cef4cfebbed7bc563499febb2626
Killion07 <wkilion@gmail.com>
2023-03-15 12:33:04 -0700

    Create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create a user, grant all rights and assigne password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
